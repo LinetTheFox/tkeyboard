b4e9a8d084757abf9a4162306f4b064b1669bfaa
LinetTheFox <linetmoreandil@gmail.com>
2022-03-20 20:57:53 +0200

    Made sketchy input logic with termion

---
+++ src/generator.rs
@@ -0,0 +1,3 @@
+pub fn generate_text(_max_words: u8) -> String {
+    "This is a test text".to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod generator;
+
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    for c in stdin.keys() {
+        write!(
+            stdout,
+            "{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::clear::CurrentLine
+        )
+        .unwrap();
+
+        match c.unwrap() {
+            Key::Ctrl('c') => break,
+            Key::Backspace => println!("BS"),
+            _ => {}
+        };
+        stdout.flush().unwrap();
+    }
+
+    // let text = generator::generate_text(1);
 }
