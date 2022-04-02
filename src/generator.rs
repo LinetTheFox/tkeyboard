5250a01c0b8a2343ad0cf9e1a08ca9776e485bb0
LinetTheFox <linetmoreandil@gmail.com>
2022-04-02 22:21:56 +0300

    added generator based off of padded list

--- src/generator.rs
+++ src/generator.rs
@@ -1,3 +1,35 @@
-pub fn generate_text(_max_words: u8) -> String {
-    "This is a test text".to_string()
+use std::fs::read_to_string;
+
+pub struct Generator {
+    padded_words: String,
+    padded_words_size: usize,
+    line_len: usize,
+}
+
+impl Generator {
+    pub fn init(file_path: &str) -> Generator {
+        let padded_words = read_to_string(file_path).unwrap();
+        let padded_words_size = padded_words.len();
+        // including '\n' at the end
+        let line_len = padded_words.chars().take_while(|c| *c != '\n').count() + 1;
+
+        Generator {
+            padded_words: padded_words,
+            padded_words_size: padded_words_size,
+            line_len: line_len,
+        }
+    }
+
+    pub fn generate(&self, word_count: u64) -> String {
+        let mut res = String::from("");
+        let max = self.padded_words_size / self.line_len;
+        for _ in 0..word_count {
+            let start = rand::random::<usize>() % max * self.line_len;
+            let end = start as usize + self.line_len;
+            let splice = self.padded_words.get(start..end).unwrap();
+            res.push_str(splice.trim());
+            res.push_str(" ");
+        }
+        res.trim_end().to_string()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,10 @@ use termion::screen::*;
 fn main() {
     let mut alt_screen = AlternateScreen::from(stdout().into_raw_mode().unwrap());
     let mut attempt: input::Attempt;
-
+    let generator = generator::Generator::init("res/words_padded.txt");
     loop {
         tui::reset(&mut alt_screen);
-        let text = generator::generate_text(0);
+        let text = generator.generate(3u64);
         attempt = input::Attempt::new_attempt(&text);
         tui::write_sample_text(text);
 
